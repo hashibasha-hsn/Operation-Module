fd05c27d89a90c21c7097949e320088a07002588
S. Hussain <s.hussain@hashibasha.com>
2026-08-05 14:43:04 +0530

    fix: register Process and Audit repositories in SubmissionsModule

--- server/org-service/src/submissions/submissions.module.ts
+++ server/org-service/src/submissions/submissions.module.ts
@@ -3,13 +3,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { SubmissionsService } from './submissions.service';
 import { SubmissionsController } from './submissions.controller';
 import { Submission } from './submission.entity';
+import { Process } from '../processes/process.entity';
+import { Audit } from '../audits/audit.entity';
 import { ProcessesModule } from '../processes/processes.module';
 import { AuditsModule } from '../audits/audits.module';
 import { EntitiesModule } from '../entities/entities.module';
 import { SupabaseStorageService } from '../noticeboard/supabase-storage.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Submission], 'org'), ProcessesModule, AuditsModule, EntitiesModule],
+  imports: [TypeOrmModule.forFeature([Submission, Process, Audit], 'org'), ProcessesModule, AuditsModule, EntitiesModule],
   controllers: [SubmissionsController],
   providers: [SubmissionsService, SupabaseStorageService],
   exports: [SubmissionsService],
